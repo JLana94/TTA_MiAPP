1685f1cd06e1fb0cb1c35e9a65042c899c2a6fb7
Josu Lana <jlana003@ikasle.ehu.eus>
2018-01-01 13:09:13 +0100

    Parte visual y lógica de presentación de las pantallas de la cafetería y comedor en el tour virtual

--- app/src/main/java/eus/ehu/tta/upv_ehutour/CafeteriaActivity.java
+++ app/src/main/java/eus/ehu/tta/upv_ehutour/CafeteriaActivity.java
@@ -1,7 +1,12 @@
 package eus.ehu.tta.upv_ehutour;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class CafeteriaActivity extends AppCompatActivity {
 
@@ -9,5 +14,38 @@ public class CafeteriaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cafeteria);
+    }
+
+    public void check(View view) {
+        int correct=2;
+        int selected=-1;
+        RadioGroup grupo=(RadioGroup) findViewById(R.id.pruebaCafeteria);
+        int opciones=grupo.getChildCount();
+        for(int i=0;i<opciones;i++)
+        {
+            if(grupo.getChildAt(i).getId()==grupo.getCheckedRadioButtonId())
+            {
+                selected=i;
+            }
+        }
+        if(selected==correct)
+        {
+            Toast.makeText(this,getResources().getString(R.string.acierto),Toast.LENGTH_SHORT).show();
+            grupo.getChildAt(correct).setBackgroundColor(getResources().getColor(R.color.verde));
+            //Aqui falta escribir que deje almaxenado en local que superado esta prueba
+        }
+        else {
+            Toast.makeText(this, getResources().getString(R.string.fallo), Toast.LENGTH_SHORT).show();
+            grupo.getChildAt(correct).setBackgroundColor(getResources().getColor(R.color.verde));
+            grupo.getChildAt(selected).setBackgroundColor(getResources().getColor(R.color.rojo));
+        }
+
+        Button avanzar=(Button) findViewById(R.id.botonAvanzarCafeteria);
+        avanzar.setEnabled(true);
+    }
+
+    public void avanzar(View view) {
+        Intent intent=new Intent(this,ComedorActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/eus/ehu/tta/upv_ehutour/ComedorActivity.java
+++ app/src/main/java/eus/ehu/tta/upv_ehutour/ComedorActivity.java
@@ -1,7 +1,10 @@
 package eus.ehu.tta.upv_ehutour;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ComedorActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class ComedorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comedor);
+    }
+    public void sacarFoto(View view) {
+
+        //Aqui estaría la llamada a la función de negocio que saca una foto
+        Button avanzar=(Button) findViewById(R.id.botonAvanzarComedor);
+        avanzar.setEnabled(true);
+    }
+
+    public void avanzar(View view) {
+        Intent intent=new Intent(this,SalaEstudiosActivity.class);
+        startActivity(intent);
     }
 }
