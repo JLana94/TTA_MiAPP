fdf443802e2896efeb2f2db62f6d03d2d71a7b52
Josu Lana <jlana003@ikasle.ehu.eus>
2018-01-01 12:13:21 +0100

    Parte visual y lógica de presentación de la pantalla inicial de la facultad de Magisterio hecha

--- app/src/main/java/eus/ehu/tta/upv_ehutour/MagisterioActivity.java
+++ app/src/main/java/eus/ehu/tta/upv_ehutour/MagisterioActivity.java
@@ -1,7 +1,9 @@
 package eus.ehu.tta.upv_ehutour;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MagisterioActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MagisterioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_magisterio);
+    }
+
+    public void comenzarTour(View view) {
+        Intent intent=new Intent(this,SecretariaActivity.class);
+        startActivity(intent);
     }
 }
