834a36a45fe31413db26bf68d423ded47794671b
Josu Lana <jlana003@ikasle.ehu.eus>
2018-01-01 11:59:28 +0100

    Parte visual y lógica de presentación de la pantalla de la parada de Bus hecha, he optado porque el boton que redirecciona a la paǵina de Bizkaibus lo haga mediante el navegador del movil, el código para hacerlo incluyendolo en la pantalla está comentado por si decido cambiarlo

--- app/src/main/java/eus/ehu/tta/upv_ehutour/BusActivity.java
+++ app/src/main/java/eus/ehu/tta/upv_ehutour/BusActivity.java
@@ -1,7 +1,12 @@
 package eus.ehu.tta.upv_ehutour;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
 
 public class BusActivity extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class BusActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bus);
+    }
+
+    public void irBizkaibus(View view) {
+
+        //Para verlo en la propia app
+        /*LinearLayout layout=(LinearLayout) findViewById(R.id.layoutBizkaibus);
+        WebView web=new WebView(this);
+        web.loadUrl(getResources().getString(R.string.URLBizkaibus));
+
+        layout.addView(web);*/
+
+        Uri uri= Uri.parse(getResources().getString(R.string.URLBizkaibus));
+        Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
+
+
     }
 }
